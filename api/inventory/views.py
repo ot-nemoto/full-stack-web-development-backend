fcf5f5b218f6ea7b04b63ff9b73eb112941645e4
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2025-04-14 22:57:50 +0000

    プログラムでデータを登録するため処理をAPIファイルに追加しよう

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -13,6 +13,14 @@ class ProductView(APIView):
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
 
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        # validationを通らなかった場合、例外を投げる
+        serializer.is_valid(raise_exception=True)
+        # 検証したデータを永続化する
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
 
 class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.all()
