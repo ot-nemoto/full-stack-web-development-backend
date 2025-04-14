83dc72fb15a4ad0c12041fdabe320b68b9c5c968
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2025-04-14 22:53:17 +0000

    APIのエンドポイントになるURLを追加しよう

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.ProductView.as_view())
+    path('products/', views.ProductView.as_view()),
+    path('products/model/',
+         views.ProductModelViewSet.as_view({'get': 'list'})),
 ]
