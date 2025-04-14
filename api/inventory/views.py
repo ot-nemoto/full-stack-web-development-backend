d8ebf87dca6f5415d0fcc20a2957e895690f7af5
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2025-04-14 22:52:02 +0000

    プログラムでデータを取得するためのAPIファイルを作成しよう

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -11,3 +12,8 @@ class ProductView(APIView):
         queryset = Product.objects.all()
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
+
+
+class ProductModelViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
