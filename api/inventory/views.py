3067be81fe1595dfa0878734f6186f91c69827ed
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2025-04-19 04:16:58 +0000

    プログラムでデータを更新するためのAPIファイルへの処理を追加しよう

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -8,16 +9,47 @@ from .serializers import ProductSerializer, PurchaseSerializer, SaleSerializer
 
 
 class ProductView(APIView):
-    def get(self, request, format=None):
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
+
+    def get_object(self, pk):
+        """
+        指定されたIDの商品の情報を取得する
+        """
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, id=None, format=None):
+        """
+        商品情報を取得する
+        """
+        if id is None:
+            queryset = Product.objects.all()
+            serializer = ProductSerializer(queryset, many=True)
+            return Response(serializer.data, status.HTTP_200_OK)
+
+        product = self.get_object(id)
+        serializer = ProductSerializer(product)
         return Response(serializer.data, status.HTTP_200_OK)
 
     def post(self, request, format=None):
+        """
+        商品情報を登録する
+        """
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+    def put(self, request, id, format=None):
+        """
+        商品情報を更新する
+        """
+        product = self.get_object(id)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
 
 
 class ProductModelViewSet(ModelViewSet):
