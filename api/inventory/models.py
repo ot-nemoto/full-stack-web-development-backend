4dd87d0fb6c762f45548de30e3db2ac78fa8c352
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2025-05-09 14:31:39 +0000

    バックエンドでモデルを定義する

--- api/inventory/models.py
+++ api/inventory/models.py
@@ -3,6 +3,15 @@ from django.db import models
 from django.utils.timezone import now
 
 
+class Status(models.IntegerChoices):
+    """
+    状態
+    """
+    SYNC = 0, '同期'
+    ASYNC_UNPROCESSED = 1, '非同期_未処理'
+    ASYNC_PROCESSED = 2, '非同期_処理済'
+
+
 class Product(models.Model):
     """
     商品
@@ -33,6 +42,19 @@ class Purchase(models.Model):
         verbose_name_plural = '仕入一覧'
 
 
+class SalesFile(models.Model):
+    """
+    売上ファイル
+    """
+    file_name = models.CharField(max_length=100, verbose_name='ファイル名')
+    status = models.IntegerField(verbose_name='状態', choices=Status.choices)
+
+    class Meta:
+        db_table = 'sales_files'
+        verbose_name = '売上ファイル'
+        verbose_name_plural = '売上ファイル一覧'
+
+
 class Sale(models.Model):
     """
     売上
@@ -41,6 +63,13 @@ class Sale(models.Model):
     quantity = models.IntegerField(
         verbose_name='数量', validators=[MinValueValidator(0)])
     sale_date = models.DateTimeField(verbose_name='売上日時', default=now)
+    import_file = models.ForeignKey(
+        SalesFile,
+        on_delete=models.CASCADE,
+        verbose_name='売上ファイルID',
+        null=True,
+        blank=True
+    )
 
     class Meta:
         db_table = 'sales'
